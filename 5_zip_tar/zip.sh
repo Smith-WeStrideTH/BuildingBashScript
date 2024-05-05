cc56bfa0b0bd8cb6e2b7c28d0330520e473ab9cd
Smith-WeStrideTH <167835418+Smith-WeStrideTH@users.noreply.github.com>
2024-05-05 09:15:18 +0000

    add 5

--- 5_zip_tar/zip.sh
+++ 5_zip_tar/zip.sh
@@ -1,10 +1,18 @@
-# Basic cmd  -> zip -[d,m,][file_name.zip] [file_name]
+# Basic cmd  -> zip [file_name.zip] [file_name]
 
+#Basic zip
+touch file_for_zip.c
 zip fie_for_zip.zip file_for_zip.c
 unzip fie_for_zip.zip
 
+#Move zip
 touch file{0..5}.txt
 zip -m myfile.zip *.txt #move into zip
 unzip myfile.zip
 
-zip -r [file_name.zip] -x [directory_name]
+
+
+
+
+
+
