c210bc8b4ed99f9099b4ae7f7a57626f65dd2eb4
Smith-WeStrideTH <167835418+Smith-WeStrideTH@users.noreply.github.com>
2024-05-05 05:15:45 +0000

    add2.1

---
+++ 2_Manipulate_data/truncate/truncate.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo "how many line do you want?"
+read line
+count=1
+while [ $count -le $line ]
+do
+	echo "$count $RANDOM" >> file.txt
+	((count++))
+done
+# (head file.txt) (head -n 3 file.txt)
+# (tail file.txt)(tail -n 1 file.txt)
+# (shuf -n 3 file.txt > smallfile.txt)
+
+
+#(cat nba_2017.csv)(head -n2 nba_2017.csv)
+
